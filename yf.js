86ee7f5d5110f3efe0cd436a17e93f71e83ea1f7
wa7chliang <wa7chliang@163.com>
2017-11-10 15:14:46 +0800

    新增加方法

--- yf.js
+++ yf.js
@@ -4,6 +4,7 @@ $.fn.switchObj = function (control, box, aClass) {
       this.box = box;
       this.aClass = aClass;
     }
+    // 鼠标点击加类 / 去除类
     switchBox.prototype.switchFunc = function () {
       var control = this.control;
       var box = this.box;
@@ -13,6 +14,29 @@ $.fn.switchObj = function (control, box, aClass) {
         $(box).eq($(this).data('num')).show().siblings(box).hide();
       })
     }
+
+    // nav导航栏下拉效果
+    switchBox.prototype.navShFunc = function () {
+      var control = this.control;
+      $(contorl).hover(function () {
+        $(this).children('ul').slideDown();
+      }, function () {
+        $(this).children('ul').slideUp();            
+      })
+    }
+
+    // 鼠标经过鼠标运动效果
+    switchBox.prototype.hoverAnimate = function (attr, start, end, time) {
+      var control = this.control;
+      if(attr == 'margin-top' || 'marginTop') {
+        $(control).hover(function () {
+          $(this).stop().animate({ marginTop: end, }, time)
+        }, function () {
+          $(this).stop().animate({ marginTop: start, }, time)
+        })
+      }
+    }
+
     return new switchBox(control, box);
   }
 
