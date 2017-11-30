292ad67f046c4735cc54fd1c1d438133f7a3bb41
wa7chliang <wa7chliang@163.com>
2017-11-30 10:02:44 +0800

    新增点击显示隐藏轮播代码

--- yf.js
+++ yf.js
@@ -4,13 +4,56 @@ $.fn.switchObj = function (control, box) {
       this.box = box;
     }
     // 鼠标点击加类 / 去除类 控制盒子显示隐藏
-    switchBox.prototype.switchFunc = function (aClass) {
+    switchBox.prototype.switchFunc = function (aClass, obj) {
       var control = this.control;
       var box = this.box;
-      $(this.control).click(function () {
-        $(this).addClass(aClass).siblings(control).removeClass(aClass);
-        $(box).eq($(this).data('num')).show().siblings(box).hide();
-      })
+      // 查看是否有自动轮播
+      var obj = obj || {};
+      obj.activeNum == null ? obj.activeNum = 0 : obj.activeNum = obj.activeNum;
+      var len = $(this.control).length
+      if(obj.autoPlay == true) {
+        var timers = ''
+        timers = setInterval(autoPlay, obj.time)
+          $(this.control).hover(function () {
+            clearInterval(timers)
+          }, function () {
+            timers = setInterval(autoPlay, obj.time)
+          })
+          $(box).hover(function () {
+            clearInterval(timers)
+          }, function () {
+            timers = setInterval(autoPlay, obj.time)
+          })
+
+          $(this.control).click(function () {
+            $(control).removeClass(aClass);
+            $($(control)[$(this).data('num')]).addClass(aClass);
+            $(box).eq($(this).data('num')).show().siblings(box).hide();
+            obj.activeNum = $(this).data('num')
+          })
+
+          function autoPlay() {
+            if(obj.activeNum < len - 1) {
+              obj.activeNum++
+              $(control).removeClass(aClass);
+              $($(control)[obj.activeNum]).addClass(aClass);
+              $(box).eq(obj.activeNum).show().siblings(box).hide();
+            } else {
+              obj.activeNum = 0;
+              $(control).removeClass(aClass);
+              $($(control)[obj.activeNum]).addClass(aClass);              
+              $(box).eq(0).show().siblings(box).hide();
+            }
+          }
+      // 无轮播正常执行代码
+      } else {
+        $(this.control).click(function () {
+          $(control).removeClass(aClass);
+          $($(control)[$(this).data('num')]).addClass(aClass);
+          $(box).eq($(this).data('num')).show().siblings(box).hide();
+        })
+      }
+
     }
 
     // nav导航栏下拉效果
